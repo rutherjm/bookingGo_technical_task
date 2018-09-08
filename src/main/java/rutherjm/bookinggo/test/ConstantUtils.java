fe368f5a203d9924eec095ef7831076a86978656
James <j.m.rutherford@lancaster.ac.uk>
2018-09-08 17:09:57 +0100

    refactored code to split the query into its own method

--- src/main/java/rutherjm/bookinggo/test/ConstantUtils.java
+++ src/main/java/rutherjm/bookinggo/test/ConstantUtils.java
@@ -1,5 +1,23 @@
 package rutherjm.bookinggo.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConstantUtils {
     public static String API_HOSTNAME = "https://techtest.rideways.com/";
+    public static Map<String, Integer> TAXI_CAPACITY = new HashMap<>();
+
+    public ConstantUtils()
+    {
+        //initiate taxi capacity map
+        TAXI_CAPACITY.put("STANDARD", 4);
+        TAXI_CAPACITY.put("EXECUTIVE", 4);
+        TAXI_CAPACITY.put("LUXURY", 4);
+        TAXI_CAPACITY.put("PEOPLE_CARRIER", 6);
+        TAXI_CAPACITY.put("LUXURY_PEOPLE_CARRIER", 6);
+        TAXI_CAPACITY.put("MINIBUS", 16);
+
+    }
+
+
 }
--- src/main/java/rutherjm/bookinggo/test/TestApplication.java
+++ src/main/java/rutherjm/bookinggo/test/TestApplication.java
@@ -1,14 +1,18 @@
 package rutherjm.bookinggo.test;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.tomcat.util.bcel.Const;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import javax.xml.ws.Response;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static rutherjm.bookinggo.test.ConstantUtils.*;
 
 @SpringBootApplication
 public class TestApplication {
@@ -20,6 +24,7 @@ public class TestApplication {
 
 		String supplierid;
 		Double pickuplat, pickuplong, dropofflat, dropofflong;
+		int minimumCapacity;
 
         AccessSupplierService as = new AccessSupplierService();
 
@@ -33,25 +38,47 @@ public class TestApplication {
         dropofflat = input.nextDouble();
         System.out.println("Drop off longitude: ");
         dropofflong = input.nextDouble();
-        
+        System.out.println("Enter minimum capacity:");
+        minimumCapacity = input.nextInt();
 
-        ResponseEntity response = (as.getResponse(new Query(supplierid, new Coordinate(pickuplat, pickuplong),new Coordinate(dropofflat, dropofflong    ))));
 
+        Query q = new Query(supplierid, new Coordinate(pickuplat, pickuplong),new Coordinate(dropofflat, dropofflong));
+        sendQuery(q, minimumCapacity);
+
+
+	}
+
+	public static void sendQuery(Query q, int minCapacity)
+    {
+        TAXI_CAPACITY.put("STANDARD", 4);
+        TAXI_CAPACITY.put("EXECUTIVE", 4);
+        TAXI_CAPACITY.put("LUXURY", 4);
+        TAXI_CAPACITY.put("PEOPLE_CARRIER", 6);
+        TAXI_CAPACITY.put("LUXURY_PEOPLE_CARRIER", 6);
+        TAXI_CAPACITY.put("MINIBUS", 16);
+
+        ResponseEntity response = new AccessSupplierService().getResponse(q);
         if (response.getStatusCode() == HttpStatus.OK){
             SuccessfulResponse sr = JsonUtils.deserialize(response.getBody().toString());
-            Arrays.sort(sr.getOptions(), new SortByPrice());
-            for (Option option: sr.getOptions())
+
+            if (sr.getOptions().length > 0)
             {
-                System.out.println(option.carType + " - " + option.price);
+                Arrays.sort(sr.getOptions(), new SortByPrice());
+                for (Option option: sr.getOptions())
+                {
+                    if (TAXI_CAPACITY.get(option.carType) >= minCapacity)
+                    {
+                        System.out.println(option.carType + " - " + option.price);
+                    }
+                }
+            }
+            else{
+                System.out.println("None available. ");
             }
         }
         else if ((response.getStatusCodeValue() == 400) || (response.getStatusCodeValue() == 500)){
             ErrorResponse er = JsonUtils.deserializeError(response.getBody().toString());
             System.out.println(er.error);
         }
-
-
-
-
-	}
+    }
 }
