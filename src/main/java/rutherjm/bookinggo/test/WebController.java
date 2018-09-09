ce3aaaf85111ccde1cbd23ce36b4ce7d59234312
James <j.m.rutherford@lancaster.ac.uk>
2018-09-09 16:43:46 +0100

    Now functioning as the requirements in step 2 specify

---
+++ src/main/java/rutherjm/bookinggo/test/WebController.java
@@ -0,0 +1,58 @@
+package rutherjm.bookinggo.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import rutherjm.bookinggo.test.JSONEntities.Coordinate;
+import sun.rmi.transport.ObjectTable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class WebController {
+    @RequestMapping(value = "/get_status")
+    public String get_status()
+    {
+        return "Application alive. ";
+    }
+    @RequestMapping(value = "/getCheapestTaxis", params = {"pickuplat", "pickuplong", "dropofflat", "dropofflong", "capacity"})
+    public ResponseEntity getCheapestTaxis(@RequestParam("pickuplat") double pickuplat,
+                                           @RequestParam("pickuplong") double pickuplong,
+                                           @RequestParam("dropofflat") double dropofflat,
+                                           @RequestParam("dropofflong") double dropofflong,
+                                           @RequestParam("capacity") int capacity)
+    {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        //Create a new Query object with the request parameters.
+        Query q = new Query(new Coordinate(pickuplat, pickuplong), new Coordinate(dropofflat, dropofflong));
+
+        //Create a new AccessSupplierService to retrieve the available options.
+        AccessSupplierService as = new AccessSupplierService();
+        ArrayList<ArrayOption> list= as.getOptionSet(q, capacity);
+
+        Map<String, Object> resultAsMap = new HashMap<>();
+        resultAsMap.put("options", list);
+
+        ObjectMapper getJson = new ObjectMapper();
+        try
+        {
+            String jsonstring = getJson.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(list);
+            return(new ResponseEntity<>(jsonstring, headers, HttpStatus.OK));
+        }
+        catch(JsonProcessingException e)
+        {
+            return(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+        }
+    }
+}
