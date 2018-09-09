dad5148b100598a9da9e4a82184c5f5e4cc77f7c
James <j.m.rutherford@lancaster.ac.uk>
2018-09-09 17:15:51 +0100

    minor tidy uo

--- src/main/java/rutherjm/bookinggo/test/SortByPrice.java
+++ src/main/java/rutherjm/bookinggo/test/SortByPrice.java
@@ -3,7 +3,6 @@ package rutherjm.bookinggo.test;
 import rutherjm.bookinggo.test.JSONEntities.JsonOption;
 
 import java.util.Comparator;
-// See https://medium.freecodecamp.org/utilizing-javas-arrays-sort-for-any-list-of-objects-e3e2db61d70b
 
 public class SortByPrice implements Comparator<JsonOption> {
     public int compare(JsonOption a, JsonOption b)
