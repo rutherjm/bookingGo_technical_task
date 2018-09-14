80aec605f175dc573b9eb253a2d10b8069cdfd47
James <j.m.rutherford@lancaster.ac.uk>
2018-09-14 17:09:02 +0100

    project restructure

---
+++ RestAPI/src/main/java/rutherjm/bookinggo/test/PrintRawResultsApplication.java
@@ -0,0 +1,11 @@
+package rutherjm.bookinggo.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintRawResultsApplication {
+    public static void main(String args[])
+    {
+        System.out.println("print raw");
+    }
+}
---
+++ src/main/java/rutherjm/bookinggo/test/PrintRawResultsApplication.java
@@ -0,0 +1,4 @@
+package rutherjm.bookinggo.test;
+
+public class PrintRawResultsApplication {
+}
