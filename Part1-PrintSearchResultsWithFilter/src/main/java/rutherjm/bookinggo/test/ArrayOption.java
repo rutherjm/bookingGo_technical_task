430f6eb0b5822756d644f4e4816124b6f3cb3e10
James <j.m.rutherford@lancaster.ac.uk>
2018-09-15 21:29:25 +0100

    Imported changes from step 2 into step 1 with filter

---
+++ Part1-PrintSearchResultsWithFilter/src/main/java/rutherjm/bookinggo/test/ArrayOption.java
@@ -0,0 +1,15 @@
+package rutherjm.bookinggo.test;
+
+
+public class ArrayOption {
+    public String carType;
+    public int price;
+    public String supplierID;
+    public ArrayOption(String type, int price, String id)
+    {
+        this.carType = type;
+        this.price = price;
+        this.supplierID = id;
+    }
+
+}
--- Part1-PrintSearchResultsWithFilter/src/main/java/rutherjm/bookinggo/test/ConstantUtils.java
+++ Part1-PrintSearchResultsWithFilter/src/main/java/rutherjm/bookinggo/test/ConstantUtils.java
@@ -1,13 +1,30 @@
 package rutherjm.bookinggo.test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ConstantUtils {
     public static final String API_HOSTNAME = "https://techtest.rideways.com/";
-    public static final String TAXI_FIRM_ID = "dave";
+    public static final List<String> TAXI_FIRM_IDS = Arrays.asList("dave", "eric", "jeff");
+    public static final List<String> TAXI_TYPES = Arrays.asList("STANDARD", "EXECUTIVE", "LUXURY", "PEOPLE_CARRIER", "LUXURY_PEOPLE_CARRIER", "MINIBUS");
 
+    public static final int MAX_INT_VALUE = 2147483647;
     public static final int TIMEOUT_MS = 2000;
     //ERRORS
     public static final String SUPPLIER_RESPONSE_NOT_RETRIEVED = "Supplier %s returned %s %s. Cause: %s ";
     public static final String RESOURCE_ACCESS_EXCEPTION_ERROR = "Resource access exception. Perhaps the request missed the timeout period of %sms";
 
+    //Taxi capacities.
+    public static final Map<String, Integer> TAXI_CAPACITY = new HashMap<String, Integer>()
+    {{
+        put("STANDARD", 4);
+        put("EXECUTIVE", 4);
+        put("LUXURY", 4);
+        put("PEOPLE_CARRIER", 6);
+        put("LUXURY_PEOPLE_CARRIER", 6);
+        put("MINIBUS", 16);
+    }};
 
 }
