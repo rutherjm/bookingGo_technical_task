218084ba2bc33105d28db80446deefc01ee63db8
James <j.m.rutherford@lancaster.ac.uk>
2018-09-15 21:40:28 +0100

    Step 1 with filter now meets new requirements.

--- Part2-RestAPI/src/main/java/rutherjm/bookinggo/test/TestApplication.java
+++ Part2-RestAPI/src/main/java/rutherjm/bookinggo/test/TestApplication.java
@@ -3,6 +3,7 @@ package rutherjm.bookinggo.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import rutherjm.bookinggo.test.JSONEntities.Coordinate;
+import rutherjm.bookinggo.test.JSONEntities.JsonOption;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,8 +11,83 @@ import java.util.Scanner;
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
-        System.out.println("Launched REST API. ");
+        Scanner input = new Scanner(System.in);
+
+        Double pickuplat, pickuplong, dropofflat, dropofflong;
+        int minimumCapacity;
+
+        //Start accepting command-line based input.
+        while(true)
+        {
+            //Get parameters for query
+            System.out.println("Pick up latitude: ");
+            pickuplat = getDoubleValue(input);
+            System.out.println("Pick up longitude: ");
+            pickuplong = getDoubleValue(input);
+            System.out.println("drop off latitude: ");
+            dropofflat = getDoubleValue(input);
+            System.out.println("Drop off longitude: ");
+            dropofflong = getDoubleValue(input);
+            System.out.println("Minimum capacity:  ");
+            minimumCapacity = getIntegerValue(input);
+
+            //Create a new query object.
+            Query q = new Query(new Coordinate(pickuplat, pickuplong),new Coordinate(dropofflat, dropofflong));
+
+            //Create a new AccessSupplierService for this static context.
+            AccessSupplierService as = new AccessSupplierService();
+            ArrayList<ArrayOption> options = as.getOptionSet(q, minimumCapacity);
+
+            //Show the results on console.
+            for (ArrayOption option: options)
+            {
+                System.out.println(option.carType + " - "  + option.supplierID + " - " + option.price);
+            }
+        }
+    }
+
+    /**
+     * Keeps looping until the Scanner input is of Double value.
+     * @param in the scanner being used on console.
+     * @return the new double value.
+     */
+    private static double getDoubleValue(Scanner in)
+    {
+        String input;
+        while(true) //keep looping until we convert a number to double successfully.
+        {
+            input = in.next();
+            try
+            {
+                return Double.parseDouble(input);
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Value not numerical! Try again.");
+            }
+        }
+    }
+    /**
+     * Keeps looping until the Scanner input is of Integer value.
+     * @param in the scanner being used on console.
+     * @return the new integer value.
+     */
+    private static int getIntegerValue(Scanner in)
+    {
+        String input;
+        while(true) //keep looping until we convert a number to integer successfully.
+        {
+            input = in.next();
+            try
+            {
+                return Integer.parseInt(input);
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Value not numerical! Try again.");
+            }
+        }
     }
 }
